2c7c610ca87e2e7a49d7e0954d4a547b8b833d4a
thiago-santanna <uzmaki_thiago@outlook.com>
2025-05-26 19:58:37 -0300

    Criado entidade - Usuario

---
+++ src/main/java/com/tsswebapps/financesiapersonal/infrastructure/adapter/out/persistence/entity/UsuarioEntity.java
@@ -0,0 +1,114 @@
+package com.tsswebapps.financesiapersonal.infrastructure.adapter.out.persistence.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class UsuarioEntity {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @Column(nullable = false, length = 100)
+        private String nome;
+        @Column(nullable = false, unique = true, length = 100)
+        private String email;
+        @Column(nullable = false)
+        private String senha;
+        @Column(length = 20)
+        private String telefone;
+
+        public UsuarioEntity() {
+        }
+
+        public UsuarioEntity(Long id, String nome, String email, String senha, String telefone) {
+                this.id = id;
+                this.nome = nome;
+                this.email = email;
+                this.senha = senha;
+                this.telefone = telefone;
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getNome() {
+                return nome;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public void setEmail(String email) {
+                this.email = email;
+        }
+
+        public String getSenha() {
+                return senha;
+        }
+
+        public void setSenha(String senha) {
+                this.senha = senha;
+        }
+
+        public String getTelefone() {
+                return telefone;
+        }
+
+        public void setTelefone(String telefone) {
+                this.telefone = telefone;
+        }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.email);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UsuarioEntity other = (UsuarioEntity) obj;
+        return Objects.equals(this.email, other.email);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("UsuarioEntity{");
+        sb.append("id=").append(id);
+        sb.append(", nome=").append(nome);
+        sb.append(", email=").append(email);
+        sb.append(", senha=").append(senha);
+        sb.append(", telefone=").append(telefone);
+        sb.append('}');
+        return sb.toString();
+    }
+        
+    
+}
